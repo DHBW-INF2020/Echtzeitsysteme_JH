eede0108b8e6059224b8fa78447102c50dbac66f
Jens Hocke <83661032+LeGlim@users.noreply.github.com>
2023-03-21 18:18:37 +0100

    diagram shows csv. still bugs there

--- classes.js
+++ classes.js
@@ -2,18 +2,19 @@ class Task {
     constructor(id, name, activities) {
         this.id = id
         this.name = name
-        this.activities = activities
+        this.activities = activities = []
     }
 }
 
 class Activity {
-    constructor(id, name, tacts, semaphoresFrom, semaphoresTo, mutexes) {
+    constructor(id, name, tacts, semaphoresFrom, semaphoresTo, mutexes, ofTaskId) {
         this.id = id
         this.name = name
         this.tacts = tacts
-        this.semaphoresFrom = semaphoresFrom
-        this.semaphoresTo = semaphoresTo
+        this.semaphoresFrom = semaphoresFrom = []
+        this.semaphoresTo = semaphoresTo = []
         this.mutexes = mutexes
+        this.ofTaskId = ofTaskId
 
         this.isActive = false
     }
--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+
 var BUTTON_IMPORT = document.getElementById("btn-import")
 var BUTTON_GENERATE = document.getElementById("btn-generate")
 var BUTTON_STEP = document.getElementById("btn-step")
@@ -6,7 +7,12 @@ var CANVAS = document.getElementById("canvas")
 
 var csvFile
 
+let Tasks = []
+let Activities = []
+let Semaphores = []
+
 function Init() {
+
 }
 
 function importCSV() {
@@ -15,11 +21,11 @@ function importCSV() {
     document.getElementById("label-import").innerHTML = "<strong>" + csvFile.name + "</strong>" + " imported"
     if (csvFile.size > 0) {
         BUTTON_GENERATE.disabled = false
-        read(showFile)
+        read(cb_read)
     }
 }
 
-function generateDiagram(array) {
+function generateDiagram(elements, connections) {
     console.log("generate diagram")
     var $ = go.GraphObject.make;
     var myDiagram = $(go.Diagram, "myDiagramDiv");
@@ -29,21 +35,32 @@ function generateDiagram(array) {
         new go.Node("Auto")  // the Shape will automatically surround the TextBlock
             // add a Shape and a TextBlock to this "Auto" Panel
             .add(new go.Shape("RoundedRectangle",
-                { strokeWidth: 0, fill: "white" })  // no border; default fill is white
+                { strokeWidth: 1, fill: "white" })  // 1 border; default fill is white
                 .bind("fill", "color"))  // Shape.fill is bound to Node.data.color
-            .add(new go.TextBlock({ margin: 8, stroke: "#333" })  // some room around the text
-                .bind("text", "key"));  // TextBlock.text is bound to Node.data.key
+            .add(new go.Panel("Table", { defaultAlignment: go.Spot.Left })
+                .add(new go.TextBlock({ text: "Name: ", row: 0, column: 0 }))
+                .add(new go.TextBlock({ margin: 8, stroke: "#333", row: 0, column: 1 })  // some room around the text
+                    .bind("text", "name"))
+                .add(new go.TextBlock({ text: "Id: ", row: 1, column: 0 }))
+                .add(new go.TextBlock({ margin: 8, stroke: "#333", row: 1, column: 1 })  // some room around the text
+                    .bind("text", "key")));
 
     var arr = [];
-    array.forEach(element => {
+    elements.forEach(element => {
         console.log(element);
-        arr.push({ key: element })
+        arr.push({ key: element.id, name: element.name })
     });
 
-    var con = [
-        { from: "1", to: "2" },
-        { from: "1", to: "3" }
-    ];
+    let con = []
+
+    connections.forEach((sempahore) => {
+        con.push({ from: sempahore.fromActivity, to: sempahore.toActivity })
+    })
+    console.log("[CONNECTIONS:]")
+    con.forEach((connection) => {
+        console.log(connection);
+    })
+
 
     myDiagram.model = new go.GraphLinksModel(arr, con)
 }
@@ -69,9 +86,91 @@ function read(callback) {
     reader.readAsText(file);
 }
 
-function showFile(result) {
-    console.log(result);
-    var rowData = result.split("\n")
+function cb_read(csvArray) {
+
+    fillTable(csvArray)
+
+    console.log(csvArray);
+    createObjects(csvArray)
+    //generateDiagram(Activities, Semaphores)
+}
+
+function createObjects(csvArray) {
+    var rows = csvArray.split("\n")
+    console.log("[ROWS]: " + rows);
+
+    // Add Semaphores
+    // Add Activities and add Semaphores to it
+    // Add Tasks and add Activities to it
+
+    // Add Mutexes?
+
+    rows.forEach((element) => {
+        let rowCol = element.split(",")
+        let type = rowCol[1], id = rowCol[0], name = rowCol[2]
+        if (type == 3) {
+            Semaphores.push(new Semaphore(id, name, rowCol[7], null, rowCol[5], rowCol[6]))
+        }
+    })
+
+    rows.forEach((element) => {
+        let rowCol = element.split(",")
+        let type = rowCol[1], id = rowCol[0], name = rowCol[2]
+
+        // Activities
+        if (type == 2) {
+            var activity = new Activity(id, name, rowCol[4], null, null, null, rowCol[3])
+            Semaphores.forEach((semaphore) => {
+                if (semaphore.toActivity == id) {
+                    activity.semaphoresFrom.push(semaphore)
+                }
+                if (semaphore.fromActivity == id) {
+                    activity.semaphoresTo.push(semaphore)
+                }
+            })
+            Activities.push(activity)
+        }
+    })
+
+    rows.forEach((element) => {
+        let rowCol = element.split(",")
+        let type = rowCol[1], id = rowCol[0], name = rowCol[2]
+        // Tasks
+        if (type == 1) {
+            var task = new Task(id, name)
+            Activities.forEach((activity) => {
+                if (activity.ofTaskId == id) {
+                    task.activities.push(activity)
+                }
+            })
+            Tasks.push(task)
+            console.log("[TASKS] added: " + name);
+        }
+    })
+
+    console.log("[TASKS]: ")
+    Tasks.forEach((task, i) => {
+        console.log(i + ": " + task.name);
+        console.log(task);
+    })
+
+    console.log("[ACTIVITIES]: ")
+    Activities.forEach((activity, i) => {
+        console.log(i + ": " + activity.name);
+        console.log(activity);
+    })
+
+    console.log("[SEMAPHORES]: ")
+    Semaphores.forEach((semaphore, i) => {
+        console.log(i + ": " + semaphore.name);
+        console.log(semaphore);
+    })
+
+}
+
+function fillTable(csvArray) {
+    console.log(fillTable.name + ": " + csvArray);
+    var rowData = csvArray.split("\n")
 
     var theadEl = document.getElementById('tblcsvdata').getElementsByTagName('thead')[0].getElementsByTagName("tr")[0];
 
@@ -79,13 +178,14 @@ function showFile(result) {
 
 
     var rowColData = rowData[0].split(',');
+    console.log("LE: " + rowColData.length);
 
     var headRow = ""
     // Loop on the row column Array
-    for (var col = 0; col < rowColData.length; col++) {
+    for (var col = 0; col < (rowColData.length - 1); col++) {
 
         headRow += "<th>" + rowColData[col] + "</th>"
-
+        console.log("LE: " + col);
     }
 
     console.log(headRow);
@@ -104,10 +204,10 @@ function showFile(result) {
         rowColData = rowData[row].split(',');
 
         // if type == 1 -> Task
-        if(rowColData[2] = 1) {
-            var task = new Task(rowColData[0], rowColData[1],)
+        if (rowColData[1] = 1) {
+            //var task = new Task(rowColData[0], rowColData[1], new Activity())
         }
-        
+
 
         ids.push(rowColData[0])
 
@@ -121,6 +221,4 @@ function showFile(result) {
         }
 
     }
-
-    generateDiagram(ids)
 }
